a4c5dbb04a8b32070851f2018cf58ec60ff05d2e
TheGradle <33810054+TheGradle@users.noreply.github.com>
2019-06-23 12:20:36 +0300

    block-sub & block-reviews

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$('a[href*="#"]').click(function(){});
+
+var $page = $('html, body');
+
+$('a[href*="#"]').click(function() {
+    $page.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 800);
+    return false;
+});
