78012db635d358445d8514f361757eeb17a6d358
TheGradle <33810054+TheGradle@users.noreply.github.com>
2019-06-24 20:31:28 +0300

    btn#toTop

--- js/script.js
+++ js/script.js
@@ -8,3 +8,17 @@ $('a[href*="#"]').click(function() {
     }, 800);
     return false;
 });
+
+$(function() {
+ $(window).scroll(function() {
+   if($(this).scrollTop() != 0) {
+    $('#toTop').fadeIn();
+   } else {
+    $('#toTop').fadeOut();
+   }
+ });
+ 
+ $('#toTop').click(function() {
+  $('body, html').animate({scrollTop:0}, 800);
+ });
+});
