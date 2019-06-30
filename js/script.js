d464051a2351ae9e330016f5d2aa26cf84b70db9
TheGradle <33810054+TheGradle@users.noreply.github.com>
2019-06-30 15:48:47 +0300

    Fix bugs

--- js/script.js
+++ js/script.js
@@ -22,3 +22,18 @@ $(function() {
   $('body, html').animate({scrollTop:0}, 800);
  });
 });
+
+ $('.screen-styles-style__demo').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.screen-styles-style-nav'
+});
+$('.screen-styles-style-nav').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.screen-styles-style__demo',
+  centerMode: true,
+  focusOnSelect: true
+});
